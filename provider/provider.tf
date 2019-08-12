7ea815e3dce807a37821471c414ab6aa106d44a8
TrainingTerraform <jade@car-part.com>
2019-08-12 09:57:59 +0000

    populate provider a little

--- provider/provider.tf
+++ provider/provider.tf
@@ -1,2 +1,18 @@
 provider "azurerm" {
+#    subscription id = 
 }
+/*
+provider "azuread"{
+}
+provider "kubernetes"{
+}
+terraform {
+    backend "azurerm" {}
+}
+provider "docker"{
+}
+provider "github"{
+}
+provider "gitlab"{
+}
+*/
